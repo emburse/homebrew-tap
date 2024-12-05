83b3f5923db832deaf8fdfb11371954ea66bc4c7
Patrick Linnane <patrick.linnane@emburse.com>
2024-12-05 07:55:57 -0800

    kustomize: add v5.4.3

---
+++ Formula/kustomize.rb
@@ -0,0 +1,67 @@
+class Kustomize < Formula
+  desc "Template-free customization of Kubernetes YAML manifests"
+  homepage "https://github.com/kubernetes-sigs/kustomize"
+  url "https://github.com/kubernetes-sigs/kustomize/archive/refs/tags/kustomize/v5.4.3.tar.gz"
+  sha256 "911e749de8d33a35ebeff50f65d0c3d79e5d1c9ff7eb9b73e29192d99b5f2444"
+  license "Apache-2.0"
+  head "https://github.com/kubernetes-sigs/kustomize.git", branch: "master"
+
+  livecheck do
+    skip "Pinned version for Emburse"
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "3d1a81fdff68e971f4a7e45945008e702c8397b86c1a5f390c8c063bc1bf5fe8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "89e6d222e8a5e9bd13ae418db39804da9a84a21fcb291683d5223abe70e9f4d3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "38f0377cccfda2718997ac62aa1e7c727e7ad979717d54a1cef793547fc15a23"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c2f682df029c1aa5c463951322c80020b0ba75c1414044af032842b3618c826a"
+    sha256 cellar: :any_skip_relocation, sonoma:         "488be64e3439c71b80eecafa30728c79e71287cb0acc3ab7474020656cf954a2"
+    sha256 cellar: :any_skip_relocation, ventura:        "d615f46ca90a431045476fb26a555ad77807fd347033165f146567d445b9b70c"
+    sha256 cellar: :any_skip_relocation, monterey:       "c4649044fed586e0a8a58ae4add96b9d0cfc99a64ab73134f18b749f847fce69"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4c71cc051fa36e9fa7abd46ad8076e07dfc0cb8b102cb7e048bca437c885c6ac"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X sigs.k8s.io/kustomize/api/provenance.version=#{name}/v#{version}
+      -X sigs.k8s.io/kustomize/api/provenance.buildDate=#{time.iso8601}
+    ]
+
+    system "go", "build", *std_go_args(ldflags:), "./kustomize"
+
+    generate_completions_from_executable(bin/"kustomize", "completion")
+  end
+
+  test do
+    assert_match "v#{version}", shell_output("#{bin}/kustomize version")
+
+    (testpath/"kustomization.yaml").write <<~YAML
+      resources:
+      - service.yaml
+      patches:
+      - path: patch.yaml
+    YAML
+    (testpath/"patch.yaml").write <<~YAML
+      apiVersion: v1
+      kind: Service
+      metadata:
+        name: brew-test
+      spec:
+        selector:
+          app: foo
+    YAML
+    (testpath/"service.yaml").write <<~YAML
+      apiVersion: v1
+      kind: Service
+      metadata:
+        name: brew-test
+      spec:
+        type: LoadBalancer
+    YAML
+    output = shell_output("#{bin}/kustomize build #{testpath}")
+    assert_match(/type:\s+"?LoadBalancer"?/, output)
+  end
+end
---
+++ README.md
@@ -0,0 +1,7 @@
+# A private tap for software used at Emburse
+
+This tap contains software used at Emburse that is not available in the main Homebrew repos. Below is a list of current Casks and Formulae
+
+## Formulae
+
+[`kustomize` v5.4.3] (Pinned due to a breaking change in v5.5.0)
